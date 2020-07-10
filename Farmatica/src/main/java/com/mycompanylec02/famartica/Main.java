d46132fa00ac9972d806b9863382583d2cd0b070
RiveraMariano <mariano.rivera.castillo@gmail.com>
2020-07-09 22:35:24 -0600

    Creación de 5 clases

---
+++ Farmatica/src/main/java/com/mycompanylec02/famartica/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompanylec02.famartica;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Grupo 4
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.menu();
+
+    } //Fin main Method
+    
+} //Fin Main class
